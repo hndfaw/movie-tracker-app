e2dba5bb95045ad15f695de355b2c01df28cf0bd
“Hindreen” <hndfaw@gmail.com>
2019-07-16 13:46:48 -0600

    Add movieRecuer content

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { moviesReducer } from './moviesReducer';
 
 const rootReducer = combineReducers({
-  firstReducer: null,
+  movies: moviesReducer,
   secondReducer: null
 });
 

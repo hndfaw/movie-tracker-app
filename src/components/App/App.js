65c96293cace7b76f411ba7cab50c1cfd0d524a1
“Hindreen” <hndfaw@gmail.com>
2019-07-16 13:18:50 -0600

    Fetching movies and genres

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component} from 'react';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import User from '../User/User'
 import './App.css';
+import { fetchFilms, fetchGenre } from '../../apiCalls'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MOVIE TRACKER</h1>
-      </header>
-      <User />
-      <MovieContainer />
-      <footer>
-        <p>Powered by TMDB</p>
-      </footer>
-    </div>
-  );
+
+
+ class App extends Component {
+
+
+   componentDidMount() {
+
+    fetchFilms().then(data => console.log(data.results))
+    fetchGenre().then(data => console.log(data.genres))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>MOVIE TRACKER</h1>
+        </header>
+        <User />
+        <MovieContainer />
+        <footer>
+          <p>Powered by TMDB</p>
+        </footer>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+// import { fetchFilms } from './apiCalls'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+

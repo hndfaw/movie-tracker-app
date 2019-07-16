11f9cf742823fa8b436a41ca4ad6bd428e1b43cd
RyanFlachman <ryan@flachman.net>
2019-07-16 08:21:56 -0600

    Add log in form files

---
+++ src/Components/SignUpForm/SignUpForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../../Containers/SignIn/SignIn'
+import SignUp from '../../Containers/SignUp/SignUp'
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <SignIn />
+      <SignUp />
+    </div>
+  )
+}
+
+export default SignUpForm;

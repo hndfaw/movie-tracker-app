9623a2f832e92fe4c95553288591da69150081e4
“Hindreen” <hndfaw@gmail.com>
2019-07-16 13:50:13 -0600

    Started favoriteReducer

---
+++ src/reducers/favoriteMoviesReducer.js
@@ -0,0 +1,10 @@
+export const favoriteReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'FAVORITE_MOVIES':
+      // return state.map(movie => {
+
+      // })
+    default:
+      return state
+  }
+}

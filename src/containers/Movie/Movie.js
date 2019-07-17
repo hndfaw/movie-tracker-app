adae34716cbb611966e37e40abf404e290daf5fb
DeMarcus Kirby <tkd024@gmail.com>
2019-07-17 16:25:01 -0600

    Add click function that dispatches to action method

--- src/containers/Movie/Movie.js
+++ src/containers/Movie/Movie.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { addFavMovie } from '../../actions'
 import { connect } from 'react-redux';
 import './Movie.css'
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, currentUser, handleClick }) => {
   const url = 'https://image.tmdb.org/t/p/w500'
   console.log(movie)
+  const checkIfLoggedIn = () => {
+    if(currentUser.loggedIn) {
+      const favMovie = {
+        movie_id: movie.id,
+        user_id: currentUser.id,
+        title: movie.title,
+        poster_path: movie.poster_path,
+        release_date: movie.release_date,
+        vote_average: movie.vote_average
+      }
+      handleClick(favMovie);
+    } else {
+      console.log('Working')
+    }
+  }
   return (
     <article className='movie'>
       <img src={url+movie.backdrop_path} alt='Movie poster'/>
@@ -14,9 +30,19 @@ const Movie = ({ movie }) => {
         <h3>Description: </h3><p>{movie.overview}</p>
         <h3>Rating: </h3><p>{movie.vote_average} out of 10</p>
       </div>
-      <button>Add this movie to favorites</button>
+      <button onClick={e => checkIfLoggedIn()}>Add this movie to favorites</button>
     </article>
   )
+  
 }
 
-export default Movie;
+
+const mapStateToProps = state => ({
+  currentUser: state.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  handleClick: (favMovie) => dispatch(addFavMovie(favMovie))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Movie);

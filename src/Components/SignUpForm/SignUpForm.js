d5456d0332f0810e6d5d44ce8482a66291d94ab5
“Hindreen” <hndfaw@gmail.com>
2019-07-20 09:00:44 -0600

    Add toggling button for sign in and sign up form

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,24 +1,37 @@
-import React, { Component} from 'react'
-import SignIn from '../../Containers/SignIn/SignIn'
-import SignUp from '../../Containers/SignUp/SignUp'
+import React, { Component} from 'react';
+import SignIn from '../../Containers/SignIn/SignIn';
+import SignUp from '../../Containers/SignUp/SignUp';
 
 class SignUpForm extends Component {
   constructor() {
     super();
     this.state = {
-      signIn: true,
+      signIn: false,
       signUp: false,
     }
   }
+
+  toggleSignInAndUp = (e) => {
+    e.target.name === 'sign-in' ?
+    this.setState({signIn: true, signUp: false}) :
+    this.setState({signIn: false, signUp: true})
+  }
+
   render() {
     
   return (
     <section>
+      <div>
+        <button onClick={this.toggleSignInAndUp} name="sign-in">Sign In</button>
+        <button onClick={this.toggleSignInAndUp} name="sign-up">Sign Up</button>
+      </div>
       {this.state.signIn && <SignIn />}
       {this.state.signUp && <SignUp />}
     </section>
   )
   }
 }
+
+
 
 export default SignUpForm;

54af6554776ea261262d80dc02cf8633cfde69ae
“Hindreen” <hndfaw@gmail.com>
2019-07-20 15:51:58 -0600

    Change movie image to main poster

--- src/containers/Movie/Movie.js
+++ src/containers/Movie/Movie.js
@@ -23,15 +23,20 @@ const Movie = ({ movie, currentUser, handleClick, getFavorites, favoriteMovies }
     }
   }
   return (
+
     <article className='movie'>
-      <img src={url+movie.backdrop_path} alt='Movie poster'/>
-      <div className='movie-info-card'>
-        <h2>{movie.title}</h2>
-        <h3>Release Date: </h3><p>{movie.release_date}</p>
-        <h3>Description: </h3><p>{movie.overview}</p>
-        <h3>Rating: </h3><p>{movie.vote_average} out of 10</p>
+      <div className="movie-detail-container">
+        <div className='movie-info-card'>
+          <h2>{movie.title}</h2>
+          <h3>Release Date: </h3><p>{movie.release_date}</p>
+          <h3>Description: </h3><p>{movie.overview}</p>
+          <h3>Rating: </h3><p>{movie.vote_average} out of 10</p>
+        </div>
+        <button onClick={e => checkIfLoggedIn()}>Add this movie to favorites</button>
       </div>
-      <button onClick={e => checkIfLoggedIn()}>Add this movie to favorites</button>
+      
+
+    <img src={url+movie.poster_path} alt='Movie poster' className="movie-poster"/>
     </article>
   )
   

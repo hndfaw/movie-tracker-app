4a7ae45f456f97487fd15ce9534efb58e153a93b
“Hindreen” <hndfaw@gmail.com>
2019-07-22 13:32:15 -0600

    Add second image to Movie

--- src/containers/Movie/Movie.js
+++ src/containers/Movie/Movie.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './Movie.css'
 import { addFavorite, getFavorites } from '..//../Thunks/favoriteThunk'
-import { NavLink } from 'react-router-dom'
 const Movie = ({ movie, currentUser, handleClick, getFavorites, favoriteMovies }) => {
   const url = 'https://image.tmdb.org/t/p/w500'
   const checkIfLoggedIn = () => {
@@ -10,7 +9,7 @@ const Movie = ({ movie, currentUser, handleClick, getFavorites, favoriteMovies }
       console.log(movie)
       const favMovie = {
         movie_id: movie.id,
-        user_id: currentUser.userDetail.id,
+        user_id: currentUser.user.id,
         title: movie.title,
         poster_path: movie.poster_path,
         release_date: movie.release_date,
@@ -18,7 +17,7 @@ const Movie = ({ movie, currentUser, handleClick, getFavorites, favoriteMovies }
         overview: movie.overview
       }
       handleClick(favMovie);
-      getFavorites(currentUser.userDetail.id)
+      getFavorites(currentUser.user.id)
     } else {
       console.log('Working')
     }
@@ -34,22 +33,17 @@ const Movie = ({ movie, currentUser, handleClick, getFavorites, favoriteMovies }
         <div className="date-rating-container">
           <p className="content-p">Rating:<span className="content-data"> {movie.vote_average} / 10</span></p>
           <p className="content-p">Release Date:<span className="content-data"> {movie.release_date}</span></p>
-          {currentUser.loggedIn && 
-          <button onClick={e => checkIfLoggedIn()}>Add this movie to favorites</button> }
-          {!currentUser.loggedIn && <NavLink to="/login" className="movie-login-link">Sign in to Favorite</NavLink>}
+          <button className="add-fav-btn" onClick={e => checkIfLoggedIn()}>Add this movie to favorites</button>
         </div>
-           
         </div>
         
-  
         <img src={url+movie.backdrop_path} alt='Movie poster' className="movie-backdrop"/>
       </div>
-      {/* {currentUser.loggedIn && 
-      <button onClick={e => checkIfLoggedIn()}>Add this movie to favorites</button> }
-      {!currentUser.loggedIn && <NavLink to="/login">Sign in to Favorite</NavLink>}
-      <img src={url+movie.poster_path} alt='Movie poster' className="movie-poster"/> */}
+
+    <img src={url+movie.poster_path} alt='Movie poster' className="movie-poster"/>
     </article>
   )
+  
 }
 
 

c5ede22e364aadedb578b2cdac82148aa23f8343
RyanFlachman <ryan@flachman.net>
2019-07-22 13:36:06 -0600

    Add display favorite movies functionality

--- src/containers/ShowFavorites/ShowFavorite.js
+++ src/containers/ShowFavorites/ShowFavorite.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import  MovieSnippet  from '../MovieSnippet/MovieSnippet'
 
-class ShowFavorites extends Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
-
-  render() {
-    return (
-      null
-    )
-  }
+const ShowFavorites = ({favorites}) => {
+  const displayFavorites = favorites[0].map( movie => {
+    return <MovieSnippet 
+      path={movie.poster_path} 
+      key={movie.id} 
+      id={movie.id}/>
+  })
+  return (
+    <section>
+      <h2>Favorite Movies!</h2>
+      {displayFavorites}
+    </section>
+  )
 }
 
 export default ShowFavorites;
